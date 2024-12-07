3659dbe2ba8c59b5ba7bcc22dbcc2a3d97220e23
Agnieszka Roguska <agnieszka.roguska@yahoo.com>
2024-12-07 13:34:42 +0100

    Leetcode Sii_v2

--- src/tasks/SiblingFinder.java
+++ src/tasks/SiblingFinder.java
@@ -15,7 +15,7 @@ public class SiblingFinder {
     public static int findLargestSibling(int firstSibling) {
         String digits = Integer.toString(firstSibling);
         Set<String> siblingsSet = new HashSet<>();
-        findPermutationsHelper("", digits, siblingsSet);
+        findSiblings("", digits, siblingsSet);
 
         int currentMax = 0;
         for (String sibling : siblingsSet) {
@@ -25,12 +25,12 @@ public class SiblingFinder {
         return currentMax;
     }
 
-    private static void findPermutationsHelper(String prefix, String remaining, Set<String> siblingsSet) {
+    private static void findSiblings(String prefix, String remaining, Set<String> siblingsSet) {
         if (remaining.isEmpty()) {
             siblingsSet.add(prefix);
         } else {
             for (int i = 0; i < remaining.length(); i++) {
-                findPermutationsHelper(prefix + remaining.charAt(i),
+                findSiblings(prefix + remaining.charAt(i),
                         remaining.substring(0, i) + remaining.substring(i + 1),
                         siblingsSet);
             }
